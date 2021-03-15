59f0f042b7d35eec401ca5a9fbd378faef673856
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2021-03-15 15:37:20 +1100

    Building HTTP middleware for JWT verification

--- routes/users.go
+++ routes/users.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/mitchrule/danksongs/actions"
 	"github.com/mitchrule/danksongs/models"
@@ -97,4 +98,48 @@ func LoginUserHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte(`{"token":"` + jwtToken + `"}`))
 	}
+}
+
+// Middleware for JWT authentication
+func AuthenticateJWTMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Returive the cookie from the http request
+		c, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				// If the cookie is not set, return an unauthorized status
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			// For any other type of error, return a bad request status
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Extract the token from the http cookie
+		tokenString := c.Value
+		log.Println(tokenString)
+
+		// Split the token string into two parts
+		tokenParts := strings.Split(tokenString, " ")
+
+		log.Panicln("Current Token Parts: ", tokenParts)
+		// #TODO: Use token parts in the action instead of the routes
+
+		if tokenString == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("Token string not found")
+		}
+
+		// Validate the token is from the user specified
+		success, err := actions.ValidateUserToken(tokenString)
+
+		// Continue the http request to the api if this succeeds
+		if err == nil && success {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+		}
+	})
 }
