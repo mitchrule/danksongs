007a949e27843b1d0156c989202765bda88ebd8d
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2021-04-06 08:31:33 +1000

    Rough Draft of the routes used
    Might change to using models over ID's

--- routes/playlists.go
+++ routes/playlists.go
@@ -105,12 +105,140 @@ func GetPlaylistHandler(w http.ResponseWriter, r *http.Request) {
 
 func DeletePlaylistHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hit DeletePlaylistHandler"))
+
+	if r.Header.Values("content-type")[0] != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		res := ErrorResponse{
+			Code:    400,
+			Message: "Incorrect content-type",
+		}
+
+		payload, err := json.Marshal(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Write(payload)
+	}
+
+	var playListID primitive.ObjectID
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	//primitive.ObjectID.JSON.Unmarshal(body, &playListID)
+
+	err = json.Unmarshal(body, &playListID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	playList, err := actions.GetPlaylist(playListID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	playListByte, err := json.Marshal(playList)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	// Returns nothing other than the success statement 
+	w.Write(playListByte)
+	w.WriteHeader(http.StatusCreated)
+
 }
 
 func AddSongHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hit AddSongHandler"))
+
+	if r.Header.Values("content-type")[0] != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		res := ErrorResponse{
+			Code:    400,
+			Message: "Incorrect content-type",
+		}
+
+		payload, err := json.Marshal(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Write(payload)
+	}
+
+	//var playListID primitive.ObjectID
+	//var songID 	   primitive.ObjectID
+
+	var ids := {
+		playListID: primitive.ObjectID,
+		songID:		primitive.ObjectID,
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	err = json.Unmarshal(body, &ids)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	//playList, err := actions.GetPlaylist(playListID)
+	success, err := actions.AddSong(ids.playListID,ids.songID)
+
+	if err != nil || !success {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	// Returns nothing other than the success statement 
+	w.WriteHeader(http.StatusCreated)
 }
 
 func RemoveSongHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hit RemoveSongHandler"))
+
+	if r.Header.Values("content-type")[0] != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		res := ErrorResponse{
+			Code:    400,
+			Message: "Incorrect content-type",
+		}
+
+		payload, err := json.Marshal(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Write(payload)
+	}
+
+	var ids := {
+		playListID: primitive.ObjectID,
+		songID:		primitive.ObjectID,
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	err = json.Unmarshal(body, &ids)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	//playList, err := actions.GetPlaylist(playListID)
+	success, err := actions.RemoveSong(ids.playListID,ids.songID)
+
+	if err != nil || !success {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	// Returns nothing other than the success statement 
+	w.WriteHeader(http.StatusCreated)
 }
