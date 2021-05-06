40bc231305b2943e91ed3d12894f87623a4a42d0
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2021-05-07 08:42:15 +1000

    routing and code example for spotify integration

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
+	github.com/zmb3/spotify v1.1.2
 	go.mongodb.org/mongo-driver v1.4.6
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 )
--- backend/routes/songs.go
+++ backend/routes/songs.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/mitchrule/danksongs/actions"
 	"github.com/mitchrule/danksongs/models"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // CreateSongHandler for creating new songs
@@ -74,7 +75,59 @@ func CreateSongHandler(w http.ResponseWriter, r *http.Request) {
 
 // TODO implement this
 func SearchSpotifyForSongsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hit GetSearchSpotifyForSongs Handler"))
+	if r.Header.Values("content-type")[0] != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		res := ErrorResponse{
+			Code:    400,
+			Message: "Incorrect content-type",
+		}
+
+		payload, err := json.Marshal(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Write(payload)
+	}
+
+	var searchTerm string
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &searchTerm)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	songs, err := actions.SearchPlaylists(searchTerm)
+	if err != nil {
+		log.Println(err)
+		if err == mongo.ErrNoDocuments {
+			w.WriteHeader(http.StatusNoContent)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	songsByte, err := json.Marshal(songs)
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	} else {
+		// Returns the playlist as a JSON
+		w.WriteHeader(http.StatusOK)
+		w.Write(songsByte)
+	}
 }
 
 func GetSongHandler(w http.ResponseWriter, r *http.Request) {
