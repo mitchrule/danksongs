644576bc676e41ca1e9b1c0e27978ffea8ce6302
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2021-08-08 22:14:46 +1000

    Work on landing page

---
+++ ui/components/DevNotes.tsx
@@ -0,0 +1,4 @@
+// Figure we could have some small notes from us on the title pages
+export default function DevNotes() {
+    return ( null );
+}
--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -6,13 +6,8 @@ import NavBar from "../components/Navbar";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <head>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-        </style>
-      </head>
       <NavBar />
+      <Component {...pageProps} />
     </>
   );
 }
