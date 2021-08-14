3c605127d9bb6993bc613b6b43baf7e696680fc0
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2021-08-14 17:27:04 +1000

    link stylesheet

--- ui/components/Title.tsx
+++ ui/components/Title.tsx
@@ -1,8 +1,8 @@
-import { Box, Typography } from "@material-ui/core";
-import { Container } from "next/app";
+import { Box, Typography, useTheme } from "@material-ui/core";
+import styles from '../styles/Title.module.css';
 
 export default function Title() {
   return (
-      <Typography>Danksongs</Typography>
+      <Typography className={styles.titleText}>Danksongs</Typography>
   );
 }
