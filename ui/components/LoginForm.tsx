eba6295756bc16e33d7f0afbb8acbadd7676311e
DanClark4 <37087415+DanClark4@users.noreply.github.com>
2021-08-16 22:27:46 +1000

    Finish prototype form

--- ui/components/LoginForm.tsx
+++ ui/components/LoginForm.tsx
@@ -1,30 +1,128 @@
-import { Button } from "@material-ui/core";
-import { Formik, Field, Form } from "formik";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Formik, Field, Form, FormikHelpers } from "formik";
 import { Container } from "next/app";
+import { Box, Button, Typography } from '@material-ui/core';
+import { useState } from 'react';
 
 const API_URL = '/';
+const FORM_DEFAULT = 'sign-in';
 
-// @TODO assign submitData type
 
-const handleSubmit = async (submitData) => {
-    const res = await fetch(API_URL, submitData);
-}
+interface SignInValues {
+    userName: string;
+    password: string;
+  }
+
+interface SignUpValues {
+    userName: string;
+    password1: string;
+    password2: string;
+  }
+  
 
 export default function LoginForm() {
-    return (
-        <Container>
+    const [formScreen, setFormScreen] = useState(FORM_DEFAULT);
+
+    const handleSignIn = async ({}) => {
+        // TODO
+        const res = await fetch(API_URL);
+    }
+    
+    const handleSignUp = async ({}) => {
+        // TODO
+        const res = await fetch(API_URL);
+    } 
+    
+    const handleClick = () => {
+        if (formScreen === 'sign-up') {
+            setFormScreen('sign-in');
+        } else {
+            setFormScreen('sign-up');
+        }
+    }
+
+    return (   
+        <Container> 
+        {/** By default let the user sign in */}
+        {formScreen === 'sign-in' && (
             <Formik
-                 initialValues={{ name: "", email: "" }}
-                 onSubmit={async (submitData) => handleSubmit(submitData)}
+                initialValues={{
+                userName: '',
+                password: '',
+                }}
+                onSubmit={(
+                values: SignInValues,
+                { setSubmitting }: FormikHelpers <SignInValues>
+                ) => {
+                setTimeout(() => {
+                    handleSignIn(values);
+                    setSubmitting(false);
+                }, 500);
+                }}
             >
                 <Form>
-                    <Field name="username" type="text" />
-                    <Field name="password" type="password"/>
-                    <Button>Sign In</Button>
+                <Typography>Username</Typography>
+                <Field id="userName" name="userName" placeholder="test" />
+
+                <Typography>Password</Typography>
+                <Field
+                    id="email"
+                    name="email"
+                    placeholder="password"
+                    type="password"
+                />
+                <Button type="submit">Log In</Button>
                 </Form>
             </Formik>
+        )}
 
+        {/** Otherwise preform the sign up here */}
+        {formScreen === 'sign-up' && (
+            <Formik
+            initialValues={{
+            userName: '',
+            password1: '',
+            password2: '',
+            }}
+            onSubmit={(
+            values: SignUpValues,
+            { setSubmitting }: FormikHelpers <SignUpValues>
+            ) => {
+            setTimeout(() => {
+                handleSignUp(values);
+                setSubmitting(false);
+            }, 500);
+            }}
+        >
+            <Form>
+            <Typography>Username</Typography>
+            <Field id="userName" name="userName" placeholder="test" />
+
+            <Typography>Password</Typography>
+            <Field
+                id="password1"
+                name="password1"
+                placeholder="password"
+                type="password"
+            />
+            <Typography>Re-Enter Password</Typography>
+            <Field
+                id="password2"
+                name="password2"
+                placeholder="password again"
+                type="password"
+            />
+            <Button type="submit">Sign Up!</Button>
+            </Form>
+            </Formik>
+        )}
+        {/* Control form display */}
+        <Box>
+            <Button onClick={handleClick}>
+                {formScreen === 'sign-up' ? 'Sign In Instead' : 'Sign Up Instead' }
+            </Button>
+        </Box>
         </Container>
-        "TODO LoginForm"
-    
+    );
 }
